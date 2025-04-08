4d5d0739ffc2d7097d00a1bc7c6f62edfb6e7ff7
Aman Kumar <amankumar001798@gmail.com>
2025-04-08 15:08:03 +0530

    done with the dashboard view page added bar charts using recharts

--- app/(main)/dashboard/_components/dashboard_view.jsx
+++ app/(main)/dashboard/_components/dashboard_view.jsx
@@ -12,6 +12,7 @@ import {
   BrainIcon,
 } from "lucide-react";
 import React from 'react'
+import { Bar, BarChart, CartesianGrid, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 function DashboardView({insights}) {
  
@@ -141,8 +142,84 @@ const nextUpdateDistance = formatDistanceToNow(
 <CardTitle className="text-sm font-medium">Salary Range by Role</CardTitle>
 <CardDescription>Displaying minimum, mean, median and maximum salaries (in thousands)</CardDescription>
 </CardHeader>
+<CardContent>
+<div className="h-[400px]">
+<ResponsiveContainer width="100%" height="100%">
+<BarChart
+  
+  data={salaryData}
+ 
+>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" />
+  <YAxis/>
+  <Tooltip
+  content={({ active, payload, label }) => {
+if(active && payload && payload.length) {
+  return (
+    <div className="bg-background  border rounded-lg p-2 shadow-md">
+<p className="font-medium">
+{label}
+</p>
+{
+  payload?.map((item) => (
+    <p key={item.name} className="text-sm"> {item.name} : ${item.value}k </p>
+  ))
+}
+    </div>
+  )
+}
+return null
+  }}/>
+  <Bar dataKey="min" fill="#BDB5D5	" name="Min Salary (K)" />
+  <Bar dataKey="median" fill="#51414F" name="Median Salary (K)" />
+  <Bar dataKey="max" fill="#9F2B68" name="Max Salary (K)" />
 
+</BarChart>
+</ResponsiveContainer>
+
+</div>
+</CardContent>
 </Card>
+
+ <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Key Industry Trends</CardTitle>
+            <CardDescription>
+              Current trends shaping the industry
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ul className="space-y-4">
+              {insights.keyTrends.map((trend, index) => (
+                <li key={index} className="flex items-start space-x-2">
+                  <div className="h-2 w-2 mt-2 rounded-full bg-primary" />
+                  <span>{trend}</span>
+                </li>
+              ))}
+            </ul>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Recommended Skills</CardTitle>
+            <CardDescription>Skills to consider developing</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-wrap gap-2">
+              {insights.recommendedSkills.map((skill) => (
+                <Badge key={skill} variant="outline">
+                  {skill}
+                </Badge>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+
+
     </div>
   )
 }
